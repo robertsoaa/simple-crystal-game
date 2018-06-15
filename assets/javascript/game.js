021a658b00c6b55ae0593a8d72383990108089f8
imclydeshelton <36463308+robertsoaa@users.noreply.github.com>
2018-06-14 23:34:06 -0400

    final push of game code before grading

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,33 @@
 //JavaScript Document
 document.getElementById("backgroundimagediv").style.backgroundImage = "url('assets/images/winterbackground.gif')";
+var audio = new Audio("assets/music/got.mp3");
+var startButtonState = false;
+$(".crystal1img").attr("disabled", false);
+//Start Button Actions
+$("#startButton").click(function() {
+  $("#startScreen").hide();
+    audio.play();
+    $("#startButton").text("RESET"); 
+    startGame()
+    $("#startButton").attr("disabled", true);
+    if (startButtonState = true) {
+      scoretracker = 0;
+      counter = 0;
+
+    }
+
+});
+function startGame() {
 //sets the initial state of the game, initial score and the global counter.
 var scoretracker = 0;
 var counter = 0;
+
+
 var ic = document.getElementById("scoretracker")
   console.log(scoretracker);
   ic.innerHTML = counter + scoretracker
+
+
 
 // Here we establish a random "targetNumber" using the math,round.
 var randomTargetNumberOptions = [19, 20, 30, 40, 50, 60, 70, 80, 90, 75, 65, 55, 56, 23, 42, 110, 120, 112, 115, 100];
@@ -16,85 +38,116 @@ var targetNumber = randomTargetNumber;
 $("#numberToGuessid").text(targetNumber);
 // Here we create a counter, we'll be using this to track the user's total.
 
-
-//Crystal Number 1
-$(".crystal1img").on("click", function() {
-
-  // This time we increase the counter by *10* each time the user clicks.
-  counter += 10;
-
-  // We then output the new counter value each time the crystal is clicked.
-  //alert("Your new score is: " + counter);
-  var sc = document.getElementById("scoretracker")
-  sc.innerHTML = counter + scoretracker
-  // Here we created some logic to "check" if the click counter matches the targetNumber.
-    // Remember, this click event will be triggered with each click.
-    // With each click the counter will increase by 10 and be re-evaluated against target.
+function winLoseLogic() {
+// Here we created some logic to "check" if the click counter matches the targetNumber.
+// Remember, this click event will be triggered with each click.
+// With each click the counter will increase by 10 and be re-evaluated against target.
     if (counter === targetNumber) {
 
       // If the numbers match we'll celebrate the user's win.
       //alert("You win!");
-      var win = document.getElementById("numberToGuessid")
-      win.innerHTML = "You win this round";
+      //var win = document.getElementById("numberToGuessid")
+      //win.innerHTML = "You win this round";
       flyingDragon()
     }
     // Here we added an "else if" condition. If the user's counter ever exceeds the targetNumber...
     else if (counter >= targetNumber) {
 
       // Then they are alerted with a loss.
-      alert("You lose!!");
+      //win.innerHTML = "You lose, unleash the ice dragon!";
       //spawn the ice dragon
       $('#icedragondiv').css("visibility", "visible");
       iceDragonFlight();   
      
     }
 
-});
+};
 
-//Crystal Number 2
-$(".crystal2img").on("click", function() {
-
+//Crystal Number 1
+$(".crystal1img").on("click", function() {
+  //randomize crystal value
+  var numberOptions = [10, 20, 30];
+  var increment = Math.floor(Math.random()*numberOptions.length);
+  var crystalValue = numberOptions[increment];
+  console.log(crystalValue);
+  
   // Each time the user clicks the crystal the counter goes up by 1.
-  counter += 5;
+  counter += crystalValue;
 
   // We then output the number of times the crystal is clicked.
   //alert("You clicked this crystal " + counter + " times!");
   var sc = document.getElementById("scoretracker")
   sc.innerHTML = counter + scoretracker
   console.log(counter)
+  winLoseLogic()
+});
+
+
+//Crystal Number 2
+$(".crystal2img").on("click", function() {
+
+  //randomize crystal value
+  var numberOptions = [1, 2, 3, 4, 5];
+  var increment = Math.floor(Math.random()*numberOptions.length);
+  var crystalValue = numberOptions[increment];
+  console.log(crystalValue);
+  
+  // Each time the user clicks the crystal the counter goes up by 1.
+  counter += crystalValue;
+
+  // We then output the number of times the crystal is clicked.
+  //alert("You clicked this crystal " + counter + " times!");
+  var sc = document.getElementById("scoretracker")
+  sc.innerHTML = counter + scoretracker
+  console.log(counter)
+  winLoseLogic()
 });
 
 //Crystal Number 3
 $(".crystal3img").on("click", function() {
 
+  //randomize crystal value
+  var numberOptions = [1, 1, 1, 1, 1, 1, 1, 1, 1, 100];
+  var increment = Math.floor(Math.random()*numberOptions.length);
+  var crystalValue = numberOptions[increment];
+  console.log(crystalValue);
+  
   // Each time the user clicks the crystal the counter goes up by 1.
-  counter += 3;
+  counter += crystalValue;
 
   // We then output the number of times the crystal is clicked.
   //alert("You clicked this crystal " + counter + " times!");
   var sc = document.getElementById("scoretracker")
   sc.innerHTML = counter + scoretracker
   console.log(counter)
+  winLoseLogic()
 });
 
 //Crystal Number 4
 $(".crystal4img").on("click", function() {
 
+  //randomize crystal value
+  var numberOptions = [10, 10, 10, 10, 5, 5, 5, 5, 5, 20];
+  var increment = Math.floor(Math.random()*numberOptions.length);
+  var crystalValue = numberOptions[increment];
+  console.log(crystalValue);
+  
   // Each time the user clicks the crystal the counter goes up by 1.
-  counter += 7;
+  counter += crystalValue;
 
   // We then output the number of times the crystal is clicked.
   //alert("You clicked this crystal " + counter + " times!");
   var sc = document.getElementById("scoretracker")
   sc.innerHTML = counter + scoretracker
   console.log(counter)
+  winLoseLogic()
 });
 
 movingWw1()
 function movingWw1() {
   var elem = document.getElementById("walker-image1");   
   var pos = 710;
-  var id = setInterval(frame, 120);
+  var id = setInterval(frame, 80);
   elem.style.top = '410px'; 
   function frame() {
     if (pos == 105) {
@@ -213,6 +266,7 @@ function flyingDragon() {
       visr5.style.visibility = 'hidden';
       visr6.style.visibility = 'hidden';
       visr7.style.visibility = 'hidden';
+      $("#startButton").attr("disabled", false);
     }
   }
 }
@@ -241,6 +295,7 @@ function iceDragonFlight() {
       $('#burningfire2').css("visibility", "visible");
       $('#burningfire3').css("visibility", "visible");
       $('#burningfire4').css("visibility", "visible");
+      $("#startButton").attr("disabled", false);
     }
   }
 }
@@ -265,16 +320,5 @@ function lastDragonflight() {
       xpos = 95;
     }
   }
-}
-randomizeCrystalValue()
-function randomizeCrystalValue() {
-// We begin by expanding our array to include four options.
-// Here we created code that selects a number either 10, 5, 3, 7.
-// We'll set this number at the start of the "game".
-var numberOptions = [10, 6, 5, 3, 7, 20, 4, 8, 9];
-for (var i = 0; i < numberOptions.length; i++) {
-var increment = numberOptions[Math.round(Math.random())];
-console.log(increment);
-
 }
 }
